2218ccc3395bb211212688e45581197b7bcfbc9b
Armadia <follietmartin@gmail.com>
2022-12-19 15:47:12 +0100

    init vuex and axios

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,16 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from "axios";
+
+//link should be on secret .env file
+const API_URL = "http://localhost:8000/api/"
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    movies: [],
+    actors: [],
+    reviews: [],
   },
   getters: {
   },
   mutations: {
+    editMovies() {
+      // state.movies = data
+    }
   },
   actions: {
+    async fetchMovies({commit}, context) {
+      console.log(context.page)
+      try{
+        const {data: {count, next, previous, results}} = await axios.get(`${API_URL}movies`, {
+          params: {
+            page: context.page
+          }
+        })
+        console.log(count, next, previous, results)
+
+        commit('editMovies')
+
+      } catch (error){
+        alert(error)
+      }
+    }
   },
   modules: {
   }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,39 @@
 <template>
-  <hello-world />
+  <v-btn @click="test">this is a test</v-btn> 
+    <!-- <hello-world /> -->
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  // import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'HomeView',
 
     components: {
-      HelloWorld,
+      // HelloWorld,
     },
+
+
+    methods: {
+      test: function() {
+        // console.log(this.$store.state)
+        this.$store.dispatch(
+          {
+            type:'fetchMovies',
+            page: 2
+          }
+         )
+      }
+    }
+
+    
+    // setup() {
+    //   const movies = this.$store.state.movies
+    //   const test = this.$store.state.testData
+
+    //   console.log(test)
+
+    //   return {movies, test}
+    // },
   }
 </script>
